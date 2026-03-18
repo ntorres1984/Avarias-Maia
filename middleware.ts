1d50702f3026d1d3d3b16165882b11f717862e98
ntorres1984 <nuno.jorge.torres@gmail.com>
2026-03-18 13:46:49 +0000

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -10,7 +10,7 @@ export async function middleware(request: NextRequest) {
     {
       cookies: {
         getAll() { return request.cookies.getAll(); },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any[]) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
           response = NextResponse.next({ request });
           cookiesToSet.forEach(({ name, value, options }) => response.cookies.set(name, value, options));
