dffda938c1e974387f026ad7591c9793f99be129
ntorres1984 <nuno.jorge.torres@gmail.com>
2026-03-23 14:39:48 +0000

    Create page.tsx

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Dashboard</h1>
+      <a href="/dashboard/nova-ocorrencia">
+        Nova Ocorrência
+      </a>
+    </div>
+  );
+}
