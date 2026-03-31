436f2d918d8269ecfd4a616f60eae319e50c4946
ntorres1984 <nuno.jorge.torres@gmail.com>
2026-03-31 09:19:48 +0100

    Create constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,31 @@
+export const CATEGORIAS = [
+  'Iluminação',
+  'AVAC',
+  'Sistema Elétrico',
+  'Água Quente Sanitária',
+  'Serralharia',
+  'Carpintaria',
+  'Águas Residuais',
+  'Águas Pluviais',
+  'Controlo de pragas ou insetos',
+  'Arranjos Exteriores',
+  'Sinalética',
+  'Deteção de Incêndio',
+  'Canalização',
+  'Inundações',
+  'Edificado',
+  'Outro',
+  'Vidros',
+]
+
+export const PRIORIDADES = ['Baixa', 'Média', 'Alta']
+
+export const IMPACTOS = ['Baixo', 'Médio', 'Alto', 'Crítico']
+
+export const ESTADOS = [
+  'Em aberto',
+  'Em análise',
+  'Em execução',
+  'Concluída',
+  'Encerrada',
+]
