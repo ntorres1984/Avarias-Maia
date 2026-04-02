a33a1f94f47ebe5cb0d9a17db3a94e792f26c690
ntorres1984 <nuno.jorge.torres@gmail.com>
2026-04-02 15:12:09 +0100

    Update page.tsx

--- src/app/registo/page.tsx
+++ src/app/registo/page.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { createClient } from '@/lib/supabase/client'
+import { createClient } from '../../lib/supabase/client'
 
 const styles = {
   page: {
