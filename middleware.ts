d23d5418a7cb495f3ed6d50c23d5497ec70b4ac8
ntorres1984 <nuno.jorge.torres@gmail.com>
2026-04-22 14:11:42 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,18 @@
-import { NextResponse, type NextRequest } from 'next/server';
-import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { NextResponse, type NextRequest } from 'next/server'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 
 type CookieToSet = {
-  name: string;
-  value: string;
-  options?: CookieOptions;
-};
+  name: string
+  value: string
+  options?: CookieOptions
+}
 
 export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
     request: {
       headers: request.headers,
     },
-  });
+  })
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -20,43 +20,43 @@ export async function middleware(request: NextRequest) {
     {
       cookies: {
         getAll() {
-          return request.cookies.getAll();
+          return request.cookies.getAll()
         },
         setAll(cookiesToSet: CookieToSet[]) {
           cookiesToSet.forEach(({ name, value }) => {
-            request.cookies.set(name, value);
-          });
+            request.cookies.set(name, value)
+          })
 
           response = NextResponse.next({
             request,
-          });
+          })
 
           cookiesToSet.forEach(({ name, value, options }) => {
-            response.cookies.set(name, value, options);
-          });
+            response.cookies.set(name, value, options)
+          })
         },
       },
     }
-  );
+  )
 
   const {
-    data: { user },
-  } = await supabase.auth.getUser();
+    data: { session },
+  } = await supabase.auth.getSession()
 
-  const isAuthPage = request.nextUrl.pathname.startsWith('/login');
-  const isPrivatePage = request.nextUrl.pathname.startsWith('/dashboard');
+  const isAuthPage = request.nextUrl.pathname.startsWith('/login')
+  const isPrivatePage = request.nextUrl.pathname.startsWith('/dashboard')
 
-  if (!user && isPrivatePage) {
-    return NextResponse.redirect(new URL('/login', request.url));
+  if (!session && isPrivatePage) {
+    return NextResponse.redirect(new URL('/login', request.url))
   }
 
-  if (user && isAuthPage) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+  if (session && isAuthPage) {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
   }
 
-  return response;
+  return response
 }
 
 export const config = {
   matcher: ['/login', '/dashboard/:path*'],
-};
+}
